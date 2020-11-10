615334c116006ef5991d7e7c0a405dff4c7992eb
xianming <2251275876@qq.com>
2020-11-10 14:29:07 +0800

    添加键盘处理

--- Tank/src/com/mashibin/tank/T.java
+++ Tank/src/com/mashibin/tank/T.java
@@ -7,6 +7,15 @@ import java.awt.event.WindowEvent;
 public class T {
 	public static void main(String[] args) {
 		TankFrame tf = new TankFrame();
+		while(true){
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			tf.repaint();
+		}
 	}
 
 }
--- Tank/src/com/mashibin/tank/TankFrame.java
+++ Tank/src/com/mashibin/tank/TankFrame.java
@@ -9,7 +9,6 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 	private int x=0,y=0;
-	
 	public TankFrame(){
 		setSize(800,600);
 		setResizable(false);
@@ -27,17 +26,58 @@ public class TankFrame extends Frame {
 	@Override
 	public void paint(Graphics g){
 		g.fillRect(x, y, 50, 50);
+		x += 200;
 	}
 	class MyKeyListener extends KeyAdapter{
-
+		private boolean bL=false,bU=false,bR=false,bD=false;
 		@Override
 		public void keyPressed(KeyEvent e) {
-			System.out.println("key pressed");
+			int key = e.getKeyCode();
+			switch(key){
+			case KeyEvent.VK_LEFT:
+				bL=true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=true;
+				break;
+			case KeyEvent.VK_UP:
+				bU=true;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=true;
+				break;
+			}
+			if (bL){
+				x-=10;
+			}
+			if (bR){
+				x+=10;
+			}
+			if (bU){
+				y-=10;
+			}
+			if (bD){
+				y+=10;
+			}
 		}
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			System.out.println("key released");
+			int key = e.getKeyCode();
+			switch(key){
+			case KeyEvent.VK_LEFT:
+				bL=false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				bR=false;
+				break;
+			case KeyEvent.VK_UP:
+				bU=false;
+				break;
+			case KeyEvent.VK_DOWN:
+				bD=false;
+				break;
+			}
 		}
 	}
 }
